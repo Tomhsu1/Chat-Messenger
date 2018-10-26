dbb68fea543de18f676957bac1a292855a633a5b
Koby Yee <ragedoggie@gmail.com>
2018-10-25 17:56:33 -0700

    gotta get toms code

--- IonicProject/src/pages/home/home.ts
+++ IonicProject/src/pages/home/home.ts
@@ -3,13 +3,34 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+import { PopoverController } from 'ionic-angular';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Component({})
+class MyPage {
+  constructor(public popoverCtrl: PopoverController) {}
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
+}
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public fAuth: AngularFireAuth) {
+
+  
+  
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,public viewCtrl: ViewController) {
+  
 
   }
 
