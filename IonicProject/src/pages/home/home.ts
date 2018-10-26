4e217020c5e0c18a27ab68b0e6881e1649d49641
Koby Yee <ragedoggie@gmail.com>
2018-10-25 17:56:33 -0700

    gotta get toms code

--- IonicProject/src/pages/home/home.ts
+++ IonicProject/src/pages/home/home.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Component({})
+class MyPage {
+  constructor(public popoverCtrl: PopoverController) {}
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
+}
+
 export class HomePage {
 
   
+  
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,public viewCtrl: ViewController) {
+  
   }
 
   myFunction() {
