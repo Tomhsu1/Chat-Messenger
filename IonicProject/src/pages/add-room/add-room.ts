b6050ad826e9fa7c0758333c74334bb82548c86b
Tom <tom268@gmail.com>
2018-11-12 20:35:36 -0800

    idk man

--- IonicProject/src/pages/add-room/add-room.ts
+++ IonicProject/src/pages/add-room/add-room.ts
@@ -18,15 +18,18 @@ import * as firebase from 'firebase';
 export class AddRoomPage {
 roomReference;
 messagesList;
-name;
 newmessage;
 roomName;
+finder;
+findRoomName;
   constructor(public navCtrl: NavController, public navParams: NavParams, public alert: AlertController, public fAuth: AngularFireAuth) {
-  this.roomReference = firebase.database().ref('privateRooms');
+  this.roomReference = firebase.database().ref().child('privateRooms');
+  this.finder = firebase.database().ref('privateRooms').equalTo('findRoomName')
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddRoomPage');
+    
   }
 
   addRoom() {
@@ -47,6 +50,40 @@ roomName;
       }
     ]
     }).present();
+    console.log(this.roomName);
+  }
+
+  send() {
+    this.roomReference.set(this.roomName);
+    //this.roomReference = firebase.database().ref("privateRooms/"+this.roomName);
+    //fix here idk how to get it to work dang it i want a child of a child to push the message to the room
+    //i also need to figure out how to add multiple childs instead of just one
+    this.roomReference.push({
+      message: this.newmessage
+    });
+  }
+
+  findRoom(){
+    this.alert.create ({
+      title: 'Find Room',
+      inputs: [{
+        name: 'roomNameKey',
+        placeholder: 'Room Name',
+      }],
+      buttons: [{
+        text: 'Find Room',
+        handler: roomNameKey => {
+          this.findRoomName = roomNameKey;
+        }
+      },
+      {
+        text: 'Cancel',
+      }
+    ]
+    }).present();
+    this.findRoomName = this.roomName;
+
+    //change here to prevent all messages from showing
     this.roomReference.on('value',data => {
       let privRm = [];
       data.forEach( data => {
@@ -59,15 +96,5 @@ roomName;
       this.messagesList = privRm;
     });
   }
-
-  send() {
-    this.roomReference.push({
-      message: this.newmessage,
-      room: this.roomName
-   });
-  }
-
-  findRoom(){
-
-  }
+  
 }
